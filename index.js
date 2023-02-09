033db1472d9c25990484921b467e5e8f9253ed5e
kanybekova eleonora <eelya250@gmail.com>
2023-02-09 14:30:37 +0600

    function that finds the area of a triangle

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // сумма двух чисел
 function sum(a, b) {
-  let result = a + b;
-  console.log(result);
+  console.log(a + b);
 }
-let x = sum(10, 40);
+sum(10, 40);
+sum(5, 5);
+
+//площадь прямоугольника
+function area(x, y) {
+  console.log("площадь прямоугольника " + x * y);
+}
+area(5, 5);
+area(30, 6);
+
