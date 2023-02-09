6b6e9384dc2b4ceaaa16de1a249fa670d7067cc9
kanybekova eleonora <eelya250@gmail.com>
2023-02-09 14:40:25 +0600

    function that outputs an even or odd number

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function area(x, y) {
 area(5, 5);
 area(30, 6);
 
+//число четное или нечетное
+function num(f) {
+  if (f % 2 === 0) {
+    console.log(f + " even");
+  } else if (f % 2 === 1) {
+    console.log(f + " odd");
+  } else {
+    console.log(f + " its not a number");
+  }
+}
+num(5);
+num(10);
+num("iphone");
